9bc3f8c91cbb5f909c99e3f199c256e0dc1758e1
Zoroot <kynshastarzzz@gmail.com>
2020-03-10 20:28:42 +0100

    Updates

---
+++ precache-manifest.466bcd3992f63113023aeef400f48419.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550b97506175bb054415581f49e6ec48",
+    "url": "/personal-store/index.html"
+  },
+  {
+    "revision": "3c05b87ca230baa1d9da",
+    "url": "/personal-store/static/css/main.7abfba83.chunk.css"
+  },
+  {
+    "revision": "a4f1ec0b82a1c047cf82",
+    "url": "/personal-store/static/js/2.255febbf.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/personal-store/static/js/2.255febbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c05b87ca230baa1d9da",
+    "url": "/personal-store/static/js/main.88119b2d.chunk.js"
+  },
+  {
+    "revision": "58fbd22a883d74df7b3a",
+    "url": "/personal-store/static/js/runtime-main.c023ea2f.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/personal-store/static/media/crown.ed28accb.svg"
+  }
+]);
